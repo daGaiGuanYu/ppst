3f9ad581fc64de8dd937272e4c43b25df2181d79
关于 <372301467@qq.com>
2020-05-25 13:15:40 +0800

    init delete

--- src/handler/delete.js
+++ src/handler/delete.js
@@ -1,5 +1,7 @@
 const { read, write } = require('../service/io')
+const parseKeystring = require('../util/parse-keystring')
 
 module.exports = function(){
-  
+  let keys = parseKeystring(process.argv[3])
+
 }
